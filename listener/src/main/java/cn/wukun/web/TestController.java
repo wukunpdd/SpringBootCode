ba173a630e6c32ab46fc9967e67015912af66d71
wukunpdd <wukunpdd@163.com>
2018-09-01 11:11:32 +0800

    监听 HTTP 会话 Session 对象

--- listener/src/main/java/cn/wukun/web/TestController.java
+++ listener/src/main/java/cn/wukun/web/TestController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/listener")
@@ -16,5 +20,39 @@ public class TestController {
     public User getUser(HttpServletRequest request) {
         ServletContext application = request.getServletContext();
         return (User) application.getAttribute("user");
+    }
+
+    /**
+     * 获取当前在线人数，该方法有bug
+     * @param request
+     * @return
+     */
+    @GetMapping("/total")
+    public String getTotalUser(HttpServletRequest request) {
+        Integer count = (Integer) request.getSession().getServletContext().getAttribute("count");
+        return "当前在线人数：" + count;
+    }
+
+    /**
+     * 该处理逻辑是让服务器记得原来那个 Session，即把原来的 sessionId 记录在浏览器中，下次再打开时，把这个 sessionId 传过去，这样服务器就不会重新再创建了。
+     * @param request
+     * @param response
+     * @return
+     */
+    @GetMapping("/total2")
+    public String getTotalUser(HttpServletRequest request, HttpServletResponse response) {
+        Cookie cookie;
+        try {
+            // 把sessionId记录在浏览器中
+            cookie = new Cookie("JSESSIONID", URLEncoder.encode(request.getSession().getId(), "utf-8"));
+            cookie.setPath("/");
+            //设置cookie有效期为2天，设置长一点
+            cookie.setMaxAge( 48*60 * 60);
+            response.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        Integer count = (Integer) request.getSession().getServletContext().getAttribute("count");
+        return "当前在线人数：" + count;
     }
 }
